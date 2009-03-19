039bc91d9c087bf0d10b35729d5261faedf25a5c
wendal1985 <wendal1985@222953f0-00bd-11de-8c4e-4bbb75a408b7>
2009-03-19 03:16:42 +0000

    重构网络层,现在改为使用线程来启动网络连接

--- trunk/XRace_V2/src/com/sa/xrace/client/network/NetWorkManager.java
+++ trunk/XRace_V2/src/com/sa/xrace/client/network/NetWorkManager.java
@@ -1,5 +1,40 @@
 package com.sa.xrace.client.network;
 
-public final class NetWorkManager {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.util.Log;
+
+import com.sa.xrace.client.toolkit.NetworkToolKit;
+import com.sa.xrace.client.toolkit.ObjectPool;
+
+public final class NetWorkManager implements Runnable{
+
+    @Override
+    public void run() {
+        try {
+            // 将Socket加入对象池
+            ObjectPool.mSocket = new Socket(InetAddress
+                    .getByName(NetworkToolKit.SERVERIP),
+                    NetworkToolKit.SERVERPORT);
+        } catch (UnknownHostException e) {
+            Log.v("-- UnknownHostException --",
+                    "-- UnknownHostException --");
+            e.printStackTrace();
+            // 是否应该退出呢?
+        } catch (IOException e) {
+            Log.v("-- IOException --", "-- IOException --");
+            e.printStackTrace();
+            // 是否应该退出呢?
+        }
+        
+        // 把PostManagerClientImp加入对象池
+        ObjectPool.mPostManager = new PostManagerClientImp();
+        // mServerListener =
+        new ServerListenerImp().start();
+    }
+    
 
 }
